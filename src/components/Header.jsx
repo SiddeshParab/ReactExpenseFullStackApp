6bb60879bfac25a09ff67071c376f69b7110f876
Siddesh Parab <119142377+SiddeshParab@users.noreply.github.com>
2023-12-19 02:53:45 +0530

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import NavBar from './NavBar';
+
+const Header = () => {
+  return (
+    <header className='wrapper bg-slate-100 h-[80px]'>
+      <NavBar />
+    </header>
+  );
+};
+
+export default Header;
